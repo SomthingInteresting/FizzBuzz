374d7b0fccdbefd122b0e7b4170799f4fecc3a9c
SomthingInteresting <qrahman115@gmail.com>
2023-03-10 16:18:44 +0000

    Moved unrefactored code to fizzbuzz_old.rb

---
+++ lib/fizzbuzz_old.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(number)
+  if number == 3 && number % 5 != 0
+    return 'fizz'
+  elsif number == 5 && number % 3 != 0
+    return 'buzz'
+  elsif number % 3 == 0 && number % 5 != 0
+    return 'fizz'
+  elsif number % 5 == 0 && number % 3 != 0
+    return 'buzz'
+  elsif number % 5 == 0 || number % 3 == 0
+    return 'fizzbuzz'
+  else 
+    return number
+  end
+end
+
+puts fizzbuzz(20)

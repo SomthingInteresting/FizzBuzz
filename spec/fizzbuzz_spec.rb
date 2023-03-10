2dcf2b27bbdbe0f1e6865339852b572c6188e234
SomthingInteresting <qrahman115@gmail.com>
2023-03-10 16:19:26 +0000

    Refactored rspec so it's DRY

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,30 +1,14 @@
 require './lib/fizzbuzz.rb'
-describe 'fizzbuzz' do 
-  it 'returns "fizzbuzz" when a multiple of 3 or 5' do 
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+RSpec.shared_examples 'a fizzbuzz number' do |number, expected_output|
+  it "returns #{expected_output.inspect} when passed #{number}" do
+    expect(fizzbuzz(number)).to eq expected_output
   end
 end
 
-describe 'fizz' do 
-  it 'returns "fizz" when passed 3' do 
-    expect(fizzbuzz(3)).to eq 'fizz'
-  end
-end
-
-describe 'buzz' do
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
-end
-
-describe 'fizz multiple 3' do 
-  it 'returns "fizz" when passed a multiple of 3' do 
-    expect(fizzbuzz(12)).to eq 'fizz'
-  end
-end
-
-describe 'buzz multiple 5' do 
-  it 'returns "buzz" when passed a multiple of 5' do 
-    expect(fizzbuzz(25)).to eq 'buzz'
-  end
+describe 'fizzbuzz' do
+  include_examples 'a fizzbuzz number', 3, 'fizz'
+  include_examples 'a fizzbuzz number', 5, 'buzz'
+  include_examples 'a fizzbuzz number', 12, 'fizz'
+  include_examples 'a fizzbuzz number', 15, 'fizzbuzz'
+  include_examples 'a fizzbuzz number', 25, 'buzz'
 end
